6ee426a26f2e3968f2966ff591d73920cd74a5cd
SerhiiAndreiko <130772110+SerhiiAndreiko@users.noreply.github.com>
2023-06-16 22:59:34 +0200

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -1,28 +1,4 @@
-# Функція для нормалізації імен
-# def normalize(file_name):
-#     # Транслітерація кирилиці на латиницю
-#     transliteration = {
-#         'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'д': 'd', 'е': 'e', 'є': 'ie', 'ж': 'zh',
-#         'з': 'z', 'и': 'i', 'і': 'i', 'ї': 'i', 'й': 'i', 'к': 'k', 'л': 'l', 'м': 'm',
-#         'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f',
-#         'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ь': '', 'ю': 'iu', 'я': 'ia',
-#         'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'H', 'Д': 'D', 'Е': 'E', 'Є': 'Ye', 'Ж': 'Zh',
-#         'З': 'Z', 'И': 'Y', 'І': 'Y', 'Ї': 'Yi', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M',
-#         'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F',
-#         'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ь': '', 'Ю': 'Yu', 'Я': 'Ya'
-#     }
 
-#     normalized = ''
-#     file_name, extension = os.path.splitext(file_name)
-#     for char in file_name:
-#         if char.isalnum() or char == '.':
-#             normalized += char
-#         elif char in transliteration:
-#             normalized += transliteration[char]
-#         else:
-#             normalized += '_'
-#     normalized += '_'
-#     return normalized
 
 CYRILLIC_SYMBOLS = "абвгґдеёєжзиіїйклмнопрстуфхцчшщъыьэюя"
 TRANSLATION = ("a", "b", "v", "h", "g", "d", "e", "e", "ie" "zh", "z",
